9501597f2548b2fbc413ab60d0660fcb8676956f
Harsh Sharma <harsh464565@gmail.com>
2023-01-16 19:50:43 +0530

    added modal.css file and created backdrop

--- public/script.js
+++ public/script.js
@@ -11,3 +11,21 @@ menu.addEventListener("click", () => {
     val = 1;
   }
 });
+
+//JS for modal functionality:
+const modal = document.getElementById("modal");
+const getStartedBtn = document.getElementById("getStartedBtn");
+const backdrop = document.getElementById("backdrop");
+const body = document.getElementsByTagName("body")[0];
+
+getStartedBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+  backdrop.style.display = "block";
+  body.style.overflowY = "hidden";
+});
+
+backdrop.addEventListener("click", () => {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+  body.style.overflowY = "auto";
+});
