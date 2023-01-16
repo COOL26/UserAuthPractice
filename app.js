9e39578a230fb7362d998ebd0056cfab66b942be
Harsh Sharma <harsh464565@gmail.com>
2023-01-16 21:19:30 +0530

    created signup page and also added some styling, also build routes for signup

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.urlencoded({ extended: false })); // for parsing user form data (text)
+
 app.set("views", path.join(__dirname, "views")); // for letting the ejs know where our .ejs files are stored.
 app.set("view engine", "ejs"); // for establishing the connection between ejs and web server
 
@@ -9,6 +11,20 @@ app.use(express.static("public")); // for serving the static CSS and JS files
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/signUp", (req, res) => {
+  res.render("signUp");
+});
+
+app.post("/signUp", (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log(username);
+  console.log(email);
+  console.log(password);
+  res.redirect("/");
 });
 
 app.listen(3000);
