b3c05eb7756545bdb512d81f3186f682254472fa
Harsh Sharma <harsh464565@gmail.com>
2023-01-17 19:15:01 +0530

    added some pasword validation and checking for existing user in db

--- app.js
+++ app.js
@@ -24,8 +24,23 @@ app.post("/signUp", async (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-  const hashedPassword = await bcrypt.hash(password, 12);
 
+  if (password.trim() < 6) {
+    console.log("password length is too short and spaces are not allowed!");
+    return res.redirect("/signUp");
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: email });
+
+  if (existingUser) {
+    console.log("user already exist please signIn");
+    return res.redirect("/signUp");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
   const user = {
     email: email,
     password: hashedPassword,
@@ -40,7 +55,28 @@ app.get("/signIn", (req, res) => {
   res.render("signIn");
 });
 
-app.post("/signIn", (req, res) => {});
+app.post("/signIn", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: email });
+
+  if (!existingUser) {
+    // checking if the user exist in our db or not
+    console.log("password not correct!");
+    return res.redirect("/signIn");
+  }
+  const isEqual = await bcrypt.compare(password, existingUser.password);
+  if (!isEqual) {
+    console.log("password not correct!");
+    return res.redirect("/signIn");
+  }
+  console.log("User is authenticated!");
+  res.redirect("/");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
